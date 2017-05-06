9c9ab133ee6c422b110bd2990b73fda54fd2a66a
jackys-95 <j_shum55@outlook.com>
2017-05-06 03:11:35 -0400

    Added prototype livematch route

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express')
+var livematch = require('./routes/livematch')
 var app = express()
 
 app.get("/", function (req, res) {
-    res.send('Hello World')
+  res.send('Hello World')
 })
 
+app.use('/livematch', livematch)
+
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000.')
+  console.log('Example app listening on port 3000.')
 })
